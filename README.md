66415ba38c271001d46b5053f7b0b90f7f457c17
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-03-08 13:26:59 -0500

    Fixed indentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# auth_scratch
+##The Odin Authentication
+
+Built this app to learn about authentication mechanism from scratch. This app is one of the exercise from ['The Odin Project'](http://www.theodinproject.com/ruby-on-rails/authentication)
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,22 +1,22 @@
 class SessionController < ApplicationController
+
   def new
   end
+
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  	log_in(user)
-  	if params[:session][:remember_me] == 1 ? remember(user) : forget(user)
-  	redirect_to user
-  	else
-      flash.now[:danger] = 'Invalid user'
-      render 'new' 	
-  	end	
-  end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      if params[:session][:remember_me] == 1 ? remember(user) : forget(user)
+        redirect_to user
+      else
+        flash.now[:danger] = 'Invalid user'
+        render 'new'
+      end
+    end
 
-  def delete
-  	log_out
-  	render 'new'
-  end
-
-
+    def delete
+      log_out
+      render 'new'
+    end
 end
